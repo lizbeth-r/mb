ffe8b6641b07cc6579fc655e2acf0c8577e96fe4
Kale e <kalee@Kales-MacBook-Pro.local>
2024-11-02 22:05:36 -0700

    mb

---
+++ posts/views.py
@@ -0,0 +1,21 @@
+from django.views.generic import (
+    CreateView,
+    ListView,
+    DetailView
+)
+from .models import Post
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+
